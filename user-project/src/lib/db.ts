cb2a8b50994ce7257e3d6faed49c443879fc0a61
Z User <z@container>
2026-08-24 16:53:51 +0000

    fix: use standard prisma connection without neon adapter

--- user-project/src/lib/db.ts
+++ user-project/src/lib/db.ts
@@ -1,19 +1,14 @@
 import { PrismaClient } from '@prisma/client'
-import { neon } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 function createPrismaClient(): PrismaClient {
-  const connectionString = process.env.DATABASE_URL
-  if (!connectionString) {
-    throw new Error('DATABASE_URL is not set. Value: ' + JSON.stringify(process.env.DATABASE_URL))
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set')
   }
-  const sql = neon(connectionString)
-  const adapter = new PrismaNeon(sql)
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 let _db: PrismaClient | null = null
@@ -29,7 +24,6 @@ function getDb(): PrismaClient {
   return _db
 }
 
-// Lazy proxy that defers connection until first actual DB call
 export const db = new Proxy({} as PrismaClient, {
   get(_, prop) {
     const client = getDb()
